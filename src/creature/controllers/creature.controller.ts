0d5b3865371e207373b6e9b7bbc998822e0eb3d0
Illia Yermackov <ermackov2003@gmail.com>
2023-07-30 22:38:40 +0300

    user controller

--- src/creature/controllers/creature.controller.ts
+++ src/creature/controllers/creature.controller.ts
@@ -1,6 +1,9 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
 import {CreatureService} from "../services/creature.service";
 import {CreateMainCreatureDto} from "../dtos/create-main-creature.dto";
+import {JwtAuthGuard} from "../../auth/guards/jwt.guard";
+import {Roles} from "../../auth/decorators/roles.decorator";
+import {RolesEnum} from "../../auth/roles/roles.enum";
 
 @Controller('creature')
 export class CreatureController {
@@ -8,11 +11,14 @@ export class CreatureController {
     constructor(private creatureService: CreatureService) {
     }
 
+
     @Get('/:id')
     getOneCreature(@Param('id') id: number) {
         return this.creatureService.findOne(id)
     }
 
+    @UseGuards(JwtAuthGuard)
+    @Roles([RolesEnum.ADMIN])
     @Post()
     addCreature(@Body('main_part') body: CreateMainCreatureDto) {
         return this.creatureService.create(body)
