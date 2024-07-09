b621f7d9c882d9cc66b796fae324b960a1727fa0
Illia Yermackov <ermackov2003@gmail.com>
2024-07-09 15:29:32 +0300

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+module.exports = {
+    apps: [
+        {
+            name: 'Viribus-initis',
+            script: 'dist/main.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '500M',
+            env: {
+                NODE_ENV: 'development',
+                JWT_SECRET: process.env.JWT_SECRET,
+                ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+                TYPEORM_USERNAME: process.env.TYPEORM_USERNAME,
+                TYPEORM_PASSWORD: process.env.TYPEORM_PASSWORD,
+                TYPEORM_DATABASE: process.env.TYPEORM_DATABASE,
+                TYPEORM_PORT: process.env.TYPEORM_PORT,
+                PORT: process.env.PORT,
+            },
+            env_production: {
+                NODE_ENV: 'production',
+                JWT_SECRET: process.env.JWT_SECRET,
+                ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+                TYPEORM_USERNAME: process.env.TYPEORM_USERNAME,
+                TYPEORM_PASSWORD: process.env.TYPEORM_PASSWORD,
+                TYPEORM_DATABASE: process.env.TYPEORM_DATABASE,
+                TYPEORM_PORT: process.env.TYPEORM_PORT,
+                PORT: process.env.PORT,
+            },
+        },
+    ],
+};
