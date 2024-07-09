f0625628dd1f52488aece74ab05be2b2d8e33964
Illia Yermackov <ermackov2003@gmail.com>
2024-07-09 15:29:47 +0300

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     apps: [
         {
-            name: 'Viribus-initis',
+            name: 'Viribus-unitis',
             script: 'dist/main.js',
             instances: 1,
             autorestart: true,
